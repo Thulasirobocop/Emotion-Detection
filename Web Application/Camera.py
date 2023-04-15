01da9b933ecfdbcccc4f2e37421e817e61a526d4
Thulasiram Saravanan <35140678+Thulasirobocop@users.noreply.github.com>
2023-04-15 13:36:56 +0530

    Add files via upload

--- Web Application/Camera.py
+++ Web Application/Camera.py
@@ -17,24 +17,26 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()        
 
-    def get_frame(self):
+    def get_frame(self,a):
             ret,i = self.video.read()
             gray=cv2.cvtColor(i,cv2.COLOR_BGR2GRAY)
             face=self.cascade.detectMultiScale(gray,1.3,5)
-            for (x,y,w,h) in face:
-                cv2.rectangle(i,(x,y),(x+w,y+h),(0,0,255),3)
+            if a:
 
-            if face !=():
-                frame = i[y:y + h, x:x + w]
-                img=cv2.imwrite('./live/test/live.jpg',frame)
-                test_generator = ImageDataGenerator(rescale=1./255).flow_from_directory(
-                    directory='./live/',target_size=(48,48),batch_size=1,
-                    shuffle=False,color_mode='grayscale')
-                predict = self.model.predict(test_generator)
-                print(predict)
-                print(predict.argmax(axis=1))
-                l=self.labels[int(predict.argmax(axis=1))]
-                cv2.putText(img=i,text=l,org=(y,x+w),fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale = 1,color= (0,0,255),thickness=3)
+                for (x,y,w,h) in face:
+                    cv2.rectangle(i,(x,y),(x+w,y+h),(0,0,255),3)
+
+                if face !=():
+                    frame = i[y:y + h, x:x + w]
+                    img=cv2.imwrite('./live/test/live.jpg',frame)
+                    test_generator = ImageDataGenerator(rescale=1./255).flow_from_directory(
+                        directory='./live/',target_size=(48,48),batch_size=1,
+                        shuffle=False,color_mode='grayscale')
+                    predict = self.model.predict(test_generator)
+                    print(predict)
+                    print(predict.argmax(axis=1))
+                    l=self.labels[int(predict.argmax(axis=1))]
+                    cv2.putText(img=i,text=l,org=(y,x+w),fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale = 1,color= (0,0,255),thickness=3)
             ret, jpeg = cv2.imencode('.jpg', i)
             return jpeg.tobytes()
--- Web Application/app.py
+++ Web Application/app.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, Response, jsonify
+from flask import Flask, render_template, Response, jsonify,request
 from Camera import VideoCamera
 import cv2
-
+a = 0
 app = Flask(__name__)
 
 video_stream = VideoCamera()
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    global a
+    if request.method == 'POST':
+        if 'button_name' in request.form:
+            if a == 0:
+                a = 1
+            elif a == 1:
+                a = 0
     return render_template('index.html')
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
+        frame = camera.get_frame(a)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-     return Response(gen(video_stream),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+     return Response(gen(video_stream), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True,port="5000")
+    app.run(host='127.0.0.1', debug=True, port="5000")
